4ca115c57830d3c87733c1a012035087ddce8555
edward7486 <edwardnux@gmail.com>
2023-12-15 13:22:14 -0800

    Add handleEditPanel to completed items

--- src/MainList/CompletedListItem.js
+++ src/MainList/CompletedListItem.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const CompletedListItem = ({ item }) => {
   
-  const { handleChecked } = useContext(DataContext);
+  const { handleChecked, handleEditPanel } = useContext(DataContext);
 
   return (
     <li className='px-2 py-2 bg-slate-100'>
@@ -18,7 +18,8 @@ const CompletedListItem = ({ item }) => {
       />
       <Link to={`/task/${item.id}`}>
         <label 
-          className="ml-2 align-middle text-slate-500 cursor-pointer line-through" 
+          className="ml-2 align-middle text-slate-500 cursor-pointer line-through"
+          onClick={(e) => handleEditPanel(true)} 
           htmlFor={ item.id } 
         >
           { item.name }
